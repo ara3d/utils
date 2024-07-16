e7d6ab8f2ca00ca2f224689e1678091c1c3af9b9
Christopher Diggins <cdiggins@gmail.com>
2024-07-16 18:36:54 -0400

    Spans and SIMD memory tool improvements

---
+++ Ara3D.Utils/ThreadUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Ara3D.Utils
+{
+    public static class ThreadUtil
+    {
+        public static void RunThreads(int threadCount, Action<int> action)
+        {
+            var threads = new List<Thread>();
+            for (var i = 0; i < threadCount; i++)
+            {
+                var thread = new Thread(startData => action((int)startData));
+                threads.Add(thread);
+                thread.Start(i);
+            }
+
+            foreach (var t in threads)
+            {
+                t.Join();
+            }
+        }
+    }
+}
