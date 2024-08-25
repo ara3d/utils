cdd72e5459f602b7adc3ec45deb62281a831b4d4
Christopher Diggins <cdiggins@gmail.com>
2024-08-25 11:21:59 -0400

    Starting to migrate to new Plato.Geometry libraries

---
+++ Ara3D.Utils/JsonUtil.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace Ara3D.Utils
+{
+    public static class JsonUtil
+    {
+        public static JsonText ToJson(this object obj)
+            => JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = true });
+
+        public static T FromJson<T>(this JsonText json)
+            => JsonSerializer.Deserialize<T>(json.Value);
+
+        public static JsonText LoadJsonText(this FilePath fp)
+            => fp.ReadAllText();
+
+        public static T WriteAsJson<T>(this FilePath fp, T obj)
+            => (fp.WriteAllText(obj.ToJson()), obj).Item2;
+
+        public static T LoadObjectAsJsonOrWriteDefault<T>(this FilePath fp) where T : new()
+            => fp.LoadObjectAsJsonOrWriteDefault<T>(new T());
+
+        public static T LoadObjectAsJsonOrWriteDefault<T>(this FilePath fp, T deflt)
+            => fp?.Exists() == true ? fp.LoadJsonText().FromJson<T>() : deflt;
+    }
+}
